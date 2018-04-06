e9654a6532e1365d245b4b0af20d16463dd1c53a
Sahib <sahib1746.cse16@chitkara.edu.in>
2018-04-07 02:29:32 +0530

    commit 4.0
    Linked App with Firebase For User Authentication and Implemented Successfully

--- app/src/main/java/production/rts/attendancemanager/LoginActivity.java
+++ app/src/main/java/production/rts/attendancemanager/LoginActivity.java
@@ -1,19 +1,28 @@
 package production.rts.attendancemanager;
 
 import android.content.Intent;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView emailTextView;
     TextView passwordTextView;
     Button loginBtn, registerBtn;
-
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +32,31 @@ public class LoginActivity extends AppCompatActivity {
         passwordTextView = findViewById(R.id.passwordText);
         loginBtn = findViewById(R.id.loginbtn);
         registerBtn = findViewById(R.id.registerbtn);
-
+        mAuth = FirebaseAuth.getInstance();
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (emailTextView.getText().toString().equalsIgnoreCase("0000")){
-                    //Toast.makeText(LoginActivity.this, emailTextView.getText(), Toast.LENGTH_SHORT).show();
-                    Intent gotoMainActivity = new Intent(LoginActivity.this,MainActivity.class);
-                    startActivity(gotoMainActivity);
+                if (emailTextView.getText().toString().equalsIgnoreCase("") || passwordTextView.getText().toString().equalsIgnoreCase("")) {
+                    Toast.makeText(LoginActivity.this, "Enter Details", Toast.LENGTH_SHORT).show();
+                } else {
+                    mAuth.signInWithEmailAndPassword(emailTextView.getText().toString(), passwordTextView.getText().toString()).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                //Sign in Completed
+                                Toast.makeText(LoginActivity.this, "Welcome to Attendace Manager", Toast.LENGTH_SHORT).show();
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Intent gotoMainActivity = new Intent(LoginActivity.this, MainActivity.class);
+                                gotoMainActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(gotoMainActivity);
+                            } else {
+                                Toast.makeText(LoginActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
                 }
-                else{
-                    Toast.makeText(LoginActivity.this, "Incorrect Credentials", Toast.LENGTH_SHORT).show();
-                }
+
             }
         });
 
@@ -49,6 +70,4 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
-
-
 }
--- app/src/main/java/production/rts/attendancemanager/RegisterActivity.java
+++ app/src/main/java/production/rts/attendancemanager/RegisterActivity.java
@@ -1,29 +1,76 @@
 package production.rts.attendancemanager;
 
 import android.content.Intent;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class RegisterActivity extends AppCompatActivity {
 
 
     Button registerbtn;
+    private FirebaseAuth mAuth;
+    EditText nameEditText, emailEditText, passwordEditText, confirmPasswordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         registerbtn = findViewById(R.id.signupbtn);
+        nameEditText = findViewById(R.id.nameedittext);
+        emailEditText = findViewById(R.id.emailedittext);
+        passwordEditText = findViewById(R.id.passwordedittext);
+        confirmPasswordEditText = findViewById(R.id.confirmpassedittext);
 
+        mAuth = FirebaseAuth.getInstance();
 
         registerbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent gotoLoginActivity = new Intent(RegisterActivity.this, LoginActivity.class);
-                startActivity(gotoLoginActivity);
+                if (nameEditText.getText().toString().equalsIgnoreCase("") || emailEditText.getText().toString().equalsIgnoreCase("") || passwordEditText.getText().toString().equalsIgnoreCase("") || confirmPasswordEditText.getText().toString().equalsIgnoreCase("")) {
+                    Toast.makeText(RegisterActivity.this, "Enter Your Crenditals", Toast.LENGTH_SHORT).show();
+                } else {
+                    mAuth.createUserWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString()).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                //Sign in Successful
+                                UserProfileChangeRequest nameUpdate = new UserProfileChangeRequest.Builder().setDisplayName(nameEditText.getText().toString()).build();
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                user.updateProfile(nameUpdate);
+                                Toast.makeText(RegisterActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                                Intent gotoLoginActivity = new Intent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(gotoLoginActivity);
+                            } else {
+                                //Sign in Unsuccessful
+                                Toast.makeText(RegisterActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+
+
             }
         });
     }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mAuth.signOut();
+    }
+
+
 }
